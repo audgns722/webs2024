34b36fe55c23a9228360e5b032444b48f44e6159
ojs104 <kmdojs@gmail.com>
2023-10-04 18:04:30 +0900

    231004

---
+++ php/board/boardWrite.php
@@ -0,0 +1,68 @@
+<?php
+    include "../connect/connect.php";
+    include "../connect/session.php";
+
+    // echo "<pre>";
+    // var_dump($_SESSION);
+    // echo "</pre>";
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 블로그 만들기</title>
+
+    <!-- CSS -->
+    <?php include "../include/head.php" ?>
+    
+</head>
+<body class="gray">
+    <?php include "../include/skip.php" ?>
+    <!-- //skip -->
+
+    <?php include "../include/header.php" ?>
+    <!-- //header -->
+
+    <main id="main" role="main">
+        <div class="intro__inner bmStyle container">
+            <div class="intro__img small">
+                <img srcset="../assets/img/board.jpg 1x,
+                            ../assets/img/board2x.jpg 2x,
+                            ../assets/img/board3x.jpg 3x" alt="소개 이미지">
+            </div>
+            <div class="intro__text">
+                <h2>게시판</h2>
+                <p>
+                    웹디자이너, 웹 퍼블리셔, 프론트앤드 개발자를 위한 게시판 입니다.<br>관련된 문의사항은 여기서 확인하세요.
+                </p>
+            </div>
+        </div>
+        <section class="board__inner container">
+            <div class="board__write">
+                <form action="boardWriteSave.php" name="boardWriteSave" method="post">
+                    <fieldset>
+                        <legend class="blind">게시글 작성하기</legend>
+                        <div>
+                            <label for="boardTitle">제목</label>
+                            <input type="text" id="boardTitle" name="boardTitle" class="input__style">
+                        </div>
+                        <div>
+                            <label for="boardContents">내용</label>
+                            <textarea name="boardContents" id="boardContents" rows="20" class="input__style"></textarea>
+                        </div>
+                        <div class="board__btns">
+                            <button type="submit" class="btn__style3">저장하기</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </section>
+    </main>
+    <!-- //main -->
+
+    <?php include "../include/footer.php" ?>
+    <!-- //footer -->
+</body>
+</html>
